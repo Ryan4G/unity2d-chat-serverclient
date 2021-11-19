b4926f46cbb370cc814dc1a4eae82e8b2a7573ab
ryan4gitee <rliang.lai@outlook.com>
2021-11-19 23:45:38 +0800

    Add a simple chat room.

---
+++ Client/Assets/Scripts/Multiplayer Game/Echo.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Echo : MonoBehaviour
+{
+    Socket socket;
+
+    public InputField inputField;
+
+    public Text text;
+
+    public Text userName;
+
+    private readonly string ip = "127.0.0.1";
+    private readonly int port = 8888;
+
+    private bool connected = false;
+
+    private byte[] readBuff = new byte[1024];
+    string recvStr = "";
+
+    private void Awake()
+    {
+        userName.text = $"UID-{new System.Random().Next(10000, 99999)}";
+    }
+
+    private void OnDestroy()
+    {
+        if (socket != null)
+        {
+            socket.Disconnect(false);
+        }
+    }
+    public void Connection()
+    {
+        socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        socket.BeginConnect(ip, port, ConnectCallback, socket);
+    }
+
+    private void ConnectCallback(IAsyncResult ar)
+    {
+        try
+        {
+            Socket socket = ar.AsyncState as Socket;
+            socket.EndConnect(ar);
+            Debug.Log("Socket Connect Successful");
+
+            socket.BeginReceive(readBuff, 0, 1024, 0, ReceiveCallback, socket);
+        }
+        catch (SocketException ex)
+        {
+            Debug.Log($"Socket Connect Failed: {ex}");
+        }
+    }
+
+    private void ReceiveCallback(IAsyncResult ar)
+    {
+        try
+        {
+            Socket socket = ar.AsyncState as Socket;
+
+            var count = socket.EndReceive(ar);
+
+            recvStr = $"{DateTime.Now:G}\n" + System.Text.Encoding.UTF8.GetString(readBuff, 0, count) + $"\n{recvStr}";
+
+            //Debug.Log(recvStr);
+
+            socket.BeginReceive(readBuff, 0, 1024, 0, ReceiveCallback, socket);
+        }
+        catch (SocketException ex)
+        {
+            Debug.Log($"Socket Receive Failed: {ex}");
+        }
+    }
+
+    public void Send()
+    {
+        string sendStr = $"{userName.text}:{inputField.text}";
+
+        byte[] sendBytes = System.Text.Encoding.UTF8.GetBytes(sendStr);
+        socket.BeginSend(sendBytes, 0, sendBytes.Length, 0, SendCallback, socket);
+    }
+
+    private void SendCallback(IAsyncResult ar)
+    {
+        try
+        {
+            Socket socket = ar.AsyncState as Socket;
+
+            var count = socket.EndSend(ar);
+
+            Debug.Log($"Socket Send {count} bytes");
+        }
+        catch (SocketException ex)
+        {
+            Debug.Log($"Socket Send Failed: {ex}");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = recvStr;
+    }
+}
---
+++ Server/EchoServer/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+
+namespace EchoServer
+{
+    class Program
+    {
+        static Socket serverSocket;
+
+        static Dictionary<Socket, ClientState> clients = new Dictionary<Socket, ClientState>();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" [Server] Started! ");
+
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            var ip = "127.0.0.1";
+            var port = 8888;
+
+            serverSocket.Bind(new IPEndPoint(IPAddress.Parse(ip), port));
+
+            serverSocket.Listen(0);
+
+            Console.WriteLine("[ Server ] Launch Successful");
+
+            serverSocket.BeginAccept(AcceptCallback, serverSocket);
+
+            Console.ReadLine();
+        }
+
+        private static void AcceptCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Console.WriteLine("[ Server ] Accept");
+
+                Socket server = ar.AsyncState as Socket;
+                Socket client = server.EndAccept(ar);
+
+                ClientState clientState = new ClientState();
+                clientState.socket = client;
+                clients.Add(client, clientState);
+
+                // wait data receive
+                client.BeginReceive(clientState.readBuff, 0, 1024, 0, ReceiveCallback, clientState);
+
+                // wait accept receive
+                serverSocket.BeginAccept(AcceptCallback, serverSocket);
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine($"Socket Accept Failed : {ex}");
+            }
+        }
+
+        private static void ReceiveCallback(IAsyncResult ar)
+        {
+            try
+            {
+                ClientState clientState = ar.AsyncState as ClientState;
+                Socket client = clientState.socket;
+
+                int count = client.EndReceive(ar);
+
+                if (count == 0)
+                {
+                    client.Close();
+                    clients.Remove(client);
+                    Console.WriteLine("Socket Close");
+                    return;
+                }
+
+                string readStr = System.Text.Encoding.UTF8.GetString(clientState.readBuff, 0, count);
+
+                Console.WriteLine($"[ Server ] {readStr}");
+
+                byte[] sendBytes = System.Text.Encoding.UTF8.GetBytes(readStr);
+
+                foreach(var clientSend in clients.Keys)
+                {
+                    clientSend.Send(sendBytes);
+                }
+
+                client.BeginReceive(clientState.readBuff, 0, 1024, 0, ReceiveCallback, clientState);
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine($"Socket Receive Failed : {ex}");
+            }
+        }
+    }
+}
